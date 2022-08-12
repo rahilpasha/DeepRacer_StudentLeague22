8a26f4874855550a74f81bdf80f3ee5ce628207d
Rahil Pasha <31114117+rahilpasha@users.noreply.github.com>
2022-08-12 15:05:10 -0700

    Create RaceV6.py

---
+++ July/RaceV6.py
@@ -0,0 +1,17 @@
+def reward_function(params):
+
+    # Create variables for each parameter used
+    steps = params["steps"]
+    progress = ["progress"] # a percentage
+    
+    speed = ["speed"]
+    
+    on_track = params["all_wheels_on_track"]
+    
+    
+    if on_track and steps > 0:
+        reward = ((speed * 5)**2) + ((progress / steps) * 100) # squares speed and adds it to the progress per step taken
+    else:
+        reward = 1e-3 # lowest reward possible
+        
+    return float(reward) # uses SAC (Soft Actor Critic) Algorithm
